279f10fbbba0497bb62da1dc4b7ffe079e2b4615
MEETKUMAR <18752823@students.latrobe.edu.au>
2019-08-05 16:38:55 +1000

    login html and Css completed

--- LabBaseAngular/e2e/src/app.e2e-spec.ts
+++ LabBaseAngular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to LabBaseAngular!');
+    expect(page.getTitleText()).toEqual('Welcome to LabBase!');
   });
 
   afterEach(async () => {
--- LabBaseAngular/src/app/app.module.ts
+++ LabBaseAngular/src/app/app.module.ts
@@ -4,21 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 import { DemographicComponent } from './demographic/demographic.component';
-import { FoodComponent } from './food/food.component';
 import { ExcerciseComponent } from './excercise/excercise.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DemographicComponent,
-    FoodComponent,
-    ExcerciseComponent
+    ExcerciseComponent,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
