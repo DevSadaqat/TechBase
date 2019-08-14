132563b440d3a5d8407fb42792f38d4a9b39b90c
MOHIT KUMAR ANGURAL <18880467@students.latrobe.edu.au>
2019-08-14 13:54:00 +1000

    Add FoodController API

---
+++ LapbaseAPI/Controllers/FoodController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LapbaseBOL;
+using LapbaseEntityFramework.Repositories;
+
+namespace LapbaseAPI.Controllers
+{
+    public class FoodController : ApiController
+    {
+        private readonly IFoodRepository foodRepository = new FoodRepository();
+
+        // GET: api/Food
+        public IHttpActionResult GetFoods(long PatientID, long OrganizationCode)
+        {
+            var foods = foodRepository.GetFoods(PatientID, OrganizationCode);
+            return Ok(foods);
+        }
+
+        // GET: api/Food/5
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult GetFood(long id)
+        {
+            var food = foodRepository.GetFoodByID(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(food);
+        }
+
+        // PUT: api/Food/5
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult PutFood(long id, Food food)
+        {
+            if (food.ID != id)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                else
+                {
+                    foodRepository.UpdateFood(food);
+                    foodRepository.Save();
+                    return Ok(food);
+                }
+
+            }
+
+        }
+
+        // POST: api/Food
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult PostFood(Food food)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            foodRepository.InsertFood(food);
+
+
+            return Ok(food);
+        }
+    }
+      
+}
