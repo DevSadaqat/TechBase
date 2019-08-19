56c87243bae94cb776a255371b970b45c4f7f53a
SIMRANJEET KAUR <18877318@students.latrobe.edu.au>
2019-08-19 13:12:02 +1000

    Add EF classes for Exercise

---
+++ LapbaseEntityFramework/Repositories/ExerciseRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LapbaseBOL;
+using System.Data.Entity;
+
+namespace LapbaseEntityFramework.Repositories
+{
+    public class ExerciseRepository : IExerciseRepository, IDisposable
+    {
+        private LapbaseContext Lb;
+        public ExerciseRepository()
+        {
+            Lb = new LapbaseContext();
+        }
+
+        public IEnumerable<Exercise> GetExercises(long PatientID, long OrganizationCode)
+        {
+            var exercises = Lb.Exercises.Where(a => a.PatientID.Equals(PatientID) && a.OrganizationCode.Equals(OrganizationCode));
+            return exercises;
+        }
+
+        public Exercise GetExerciseByID(long Id)
+        {
+            return Lb.Exercises.Find(Id);
+        }
+
+        public void InsertExercise(Exercise exercise)
+        {
+            Lb.Exercises.Add(exercise);
+            Save();
+        }
+
+        public void DeleteExercise(long id)
+        {
+            Exercise exercise = Lb.Exercises.Find(id);
+            Lb.Exercises.Remove(exercise);
+        }
+
+        public void UpdateExercise(Exercise exercise)
+        {
+            Lb.Entry(exercise).State = EntityState.Modified;
+            Lb.Configuration.ValidateOnSaveEnabled = false;
+            Save();
+            Lb.Configuration.ValidateOnSaveEnabled = true;
+        }
+
+        public void Save()
+        {
+            Lb.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        //Used to clean up and free unmanaged resources
+        
+        protected virtual void Dispose (bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    Lb.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ LapbaseEntityFramework/Repositories/IExerciseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LapbaseBOL;
+
+namespace LapbaseEntityFramework.Repositories
+{
+    public interface IExerciseRepository : IDisposable
+    {
+        IEnumerable<Exercise> GetExercises(long PatientID, long OrganizationCode);
+        Exercise GetExerciseByID(long id);
+        void InsertExercise(Exercise exercise);
+        void DeleteExercise(long id);
+        void UpdateExercise(Exercise exercise);
+        void Save();
+    }
+}
