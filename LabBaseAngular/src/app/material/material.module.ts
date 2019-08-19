11aeb02c519b04a6e0f632a68c06b11e5bd8c5a4
MEETKUMAR NALINKUMAR PATEL <18753823@students.latrobe.edu.au>
2019-08-19 09:07:20 +0000

    Initial Commit

--- LabBaseAngular/src/app/material/material.module.ts
+++ LabBaseAngular/src/app/material/material.module.ts
@@ -16,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     Material.MatInputModule,
     Material.MatCardModule,
     Material.MatBadgeModule,
+    Material.MatToolbarModule,
     BrowserAnimationsModule,
   ]
 })
