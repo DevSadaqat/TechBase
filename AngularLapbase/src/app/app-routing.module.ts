d8f114f9437d5780fe814b2dde9c4650a7f660ce
MEETKUMAR NALINKUMAR PATEL <meet9713@gmail.com>
2019-08-29 14:11:50 +0000

    ADD  SER-PANEL 4 LAYOUTS AND THEMES & FIXING LOGO

--- AngularLapbase/src/app/app-routing.module.ts
+++ AngularLapbase/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ export const Approutes: Routes = [
     path: 'login',
     redirectTo: '/authentication/login'
   },
-  { path: 'login', redirectTo: '/authentication/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/authentication/login', pathMatch: 'full' },
   {path: 'authentication', loadChildren: './authentication/authentication.module#AuthenticationModule'},
     {path: '', component: FullComponent,
     children: [
@@ -18,19 +18,7 @@ export const Approutes: Routes = [
       { path: 'dashboard', loadChildren: './dashboards/dashboard.module#DashboardModule'},
       { path: 'apps', loadChildren: './apps/apps.module#AppsModule' },
       { path: 'component', loadChildren: './component/component.module#ComponentsModule' },
-      /*{ path: 'cards', loadChildren: './cards/cards.module#CardsModule' },
-      /*{ path: 'starter', loadChildren: './starter/starter.module#StarterModule'},
-      { path: 'icons', loadChildren: './icons/icons.module#IconsModule' },
-      { path: 'forms', loadChildren: './form/forms.module#FormModule' },
-      { path: 'tables', loadChildren: './table/tables.module#TablesModule' },
-      { path: 'charts', loadChildren: './charts/charts.module#ChartModule' },
-      { path: 'widgets', loadChildren: './widgets/widgets.module#WidgetsModule'},
-      { path: 'ecom', loadChildren: './ecommerce/ecom.module#EcomModule' },
-      { path: 'timeline', loadChildren: './timeline/timeline.module#TimelineModule'},
-      { path: 'extra-component', loadChildren: './extra-component/extra-component.module#ExtraComponentModule' },*/
-	    /*{ path: 'apps/email', loadChildren: './apps/email/mail.module#MailModule' },
-      { path: 'maps', loadChildren: './maps/maps.module#MapsModule' },
-      { path: 'sample-pages', loadChildren: './sample-pages/sample-pages.module#SamplePagesModule'}*/
+      
     ]
   },
   {
